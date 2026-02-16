d7e463ff29461c33e7795b05e0695b6859e23687
Jack Rosenberg <mail@jackr.eu>
2026-02-16 18:21:27 +0100

    command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{fs, env};
+
 fn main() {
-    println!("Hello, world!");
+    // take in command line args
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let path = &args[1];
+    println!("Searching path '{}'", path);
+
+    let contents = fs::read_to_string(path)
+        .expect("Unable to read path ");
+
+    println!("File contains \n '{}'", contents);
+
 }
